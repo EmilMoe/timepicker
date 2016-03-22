07429e6fa740d6da77ed7b1022ce170fea5042e3
Emil Moe <emil@moegroup.dk>
2016-03-22 14:45:02 +0700

    Fixing bugs where menu not appear

--- src/timepicker.js
+++ src/timepicker.js
@@ -115,7 +115,6 @@ var timepicker = {
     },
     showHour: function(timepicker, interval, min, max, callback)
     {
-        var height = null;
         ul     = this.make(timepicker);
 
         for (var i = min; i < max; i = i + interval)
@@ -125,9 +124,6 @@ var timepicker = {
             li.setAttribute('data-value', this.pad(i));
             li.appendChild(document.createTextNode(this.pad(i) +':00'));
             ul.appendChild(li);
-
-            if (height == null)
-                height = li.clientHeight;
 
             li.onmouseover = function()
             {
@@ -148,11 +144,6 @@ var timepicker = {
                 callback(ul, this.getAttribute('data-value'));
             };
         }
-
-        if (this.time.hour == null)
-            ul.scrollTop = height * this.option('default');
-        else
-            ul.scrollTop = this.time.hour * height;
     },
     showMinute: function(list, interval, max, hour, callback)
     {
